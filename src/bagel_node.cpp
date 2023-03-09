bc88600eb5db41457b26f0226c8c3567d7ff0875
Micaela Angeli <1470630+mica-angeli@users.noreply.github.com>
2023-03-09 12:59:26 +0000

    Added argparse library

--- src/bagel_node.cpp
+++ src/bagel_node.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <argparse/argparse.hpp>
 
-int main(void)
-{
+int main(int argc, char *argv[]) {
+  argparse::ArgumentParser parser("bagel");
+
+  try {
+    parser.parse_args(argc, argv);
+  }
+  catch (const std::runtime_error& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << parser;
+    std::exit(1);
+  }
+
     std::cout << "Hello World!" << std::endl;
     return 0;
 }

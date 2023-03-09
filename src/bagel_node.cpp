75a8a5b834b5e484c8ec4d7e6a5c395d3bd8dedf
Micaela Angeli <1470630+mica-angeli@users.noreply.github.com>
2023-03-09 13:38:56 +0000

    Added basic rosbag support

--- src/bagel_node.cpp
+++ src/bagel_node.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 #include <argparse/argparse.hpp>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
 
 int main(int argc, char *argv[]) {
   argparse::ArgumentParser parser("bagel");
+
+  parser.add_argument("bag")
+    .help("Bagfile to process");
 
   try {
     parser.parse_args(argc, argv);
@@ -13,6 +18,19 @@ int main(int argc, char *argv[]) {
     std::exit(1);
   }
 
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+  std::cout << parser.get<std::string>("bag") << std::endl;
+
+  rosbag::Bag bag;
+
+  const auto bag_path = parser.get<std::string>("bag");
+  bag.open(bag_path, rosbag::bagmode::Read);
+
+  rosbag::View view(bag);
+
+  ros::Time bag_start_time = view.getBeginTime();
+  ros::Time bag_end_time = view.getEndTime();
+
+  std::cout << "ROS bag time: " << (bag_end_time - bag_start_time).toSec() << " (s)" << std::endl;
+  bag.close();
+  return 0;
 }
